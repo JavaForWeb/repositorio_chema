55d1a85250759b63ad1468f0eda85ed3e8ddf1d1
Iracema Peralta <iperaltao@gmail.com>
2016-10-08 10:37:25 -0400

    se borro el class y se agrego java

---
+++ HelloWord.java
@@ -0,0 +1,12 @@
+/* Este es un simple programa de java
+	Este archivo debe llamarse HelloWord.java
+*/
+
+class HelloWord 
+{
+	// Un programa de Java comienza con una llamada al main()
+	public static void main(String args[]) 
+	{
+		System.out.println("Hello from Java!!");
+	}
+}
